d3c7212d9c6a3cf60b21b7553d195139125f57b7
Jrey433 <jrey.getty@students.msswarriors.org>
2026-04-01 11:36:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-// Initialized Value - variable declared and given a value
-let initializedValue = "Hello World";
-document.getElementById("initialized-value").textContent = initializedValue;
+let courseLength = 20;
 
-// Unassigned Value - variable declared but no value assigned yet
-let unassignedValue;
-document.getElementById("unassigned-value").textContent = unassignedValue;
-
-// Assigned Value - variable declared and assigned
-let assignedValue = 42;
-document.getElementById("assigned-value").textContent = assignedValue;
-
-// Reassigned Value - variable given a new value
-let reassignedValue = "First";
-reassignedValue = "Second";
-document.getElementById("reassigned-value").textContent = reassignedValue;
+document.getElementById("result").innerHTML = courseLength;
